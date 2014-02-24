7c57d647fae23f5967008550fa60b202929441b4
saasbook <saasbook@saasbook.(none)>
2014-02-24 23:57:36 +0530

    First Splash page for BrainStorm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bsmain::Application.config.secret_token = 'a7151f7e3a99f37c5b87a9c040e52911077f72a507202cdb7a05bc570137e2941cf84e15ae9c767672dc6f2879cb45818273a56aee51435334ece95655e3dd62'
